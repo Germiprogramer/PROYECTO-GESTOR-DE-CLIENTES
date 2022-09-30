4c75df5df73865a8f7643f3fa6e90771360c5c9a
Germiprogramer <llorentemunozgerman@gmail.com>
2022-09-30 19:52:04 +0100

    csv de clientes iniciales y funciones cliente

--- database.py
+++ database.py
@@ -15,3 +15,49 @@ class Cliente:
 
     def to_dict(self):
         return {'dni': self.dni, 'nombre': self.nombre, 'apellido': self.apellido}
+
+class Clientes:
+
+    lista = []
+
+    #te guarda únicamente la última vez que se ejecuta la función, ocupando menos espacio
+    @staticmethod
+    def buscar(dni):
+        for cliente in Clientes.lista:
+            if cliente.dni == dni:
+                return cliente
+
+    def crear(dni, nombre, apellido):
+        cliente = Cliente(dni, nombre, apellido)
+        Clientes.lista.append(cliente)
+        #la función guardar se define más adelante
+        Clientes.guardar()
+        return cliente
+
+    @staticmethod
+    def modificar(dni, nombre, apellido):
+        #enumerate va recorriendo todos y cada uno de los elementos de la lista
+        for indice, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                Clientes.lista[indice].nombre = nombre
+                Clientes.lista[indice].apellido = apellido
+                Clientes.guardar()
+                return Clientes.lista[indice]
+
+    @staticmethod
+    def borrar(dni):
+        for indice, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                cliente = Clientes.lista.pop(indice)
+                Clientes.guardar()
+                return cliente
+
+    @staticmethod
+    def guardar():
+        with open(config.DATABASE_PATH, 'w', newline='\n') as fichero:
+            writer = csv.writer(fichero, delimiter=';')
+            for cliente in Clientes.lista:
+                writer.writerow((cliente.dni, cliente.nombre, cliente.apellido))
+
+
+    

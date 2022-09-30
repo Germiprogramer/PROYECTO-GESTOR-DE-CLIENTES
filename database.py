73bc9571a2827eadefa572a90473b1cdfe629a57
Germiprogramer <llorentemunozgerman@gmail.com>
2022-09-30 19:23:02 +0100

    commit inicial

---
+++ database.py
@@ -0,0 +1,17 @@
+#manejar la gestión de los datos
+
+import csv
+import config
+
+
+class Cliente:
+    def __init__(self, dni, nombre, apellido):
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
+
+    def __str__(self):
+        return f"({self.dni}) {self.nombre} {self.apellido}"
+
+    def to_dict(self):
+        return {'dni': self.dni, 'nombre': self.nombre, 'apellido': self.apellido}

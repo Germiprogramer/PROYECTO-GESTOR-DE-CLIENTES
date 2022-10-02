3b3067277fa688640700700257ea13bfdfa462c9
Germiprogramer <llorentemunozgerman@gmail.com>
2022-10-02 16:42:47 +0100

    commit

--- helpers.py
+++ helpers.py
@@ -26,3 +26,13 @@ def dni_valido(dni, lista):
             print("DNI utilizado por otro cliente.")
             return False
     return True
+
+def dni_valido(dni, lista):
+    if not re.match('[0-9]{2}[A-Z]$', dni):
+        print("DNI incorrecto, debe cumplir el formato.")
+        return False
+    for cliente in lista:
+        if cliente.dni == dni:
+            print("DNI utilizado por otro cliente.")
+            return False
+    return True
